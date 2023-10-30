770ed4ed22b9e343c8eed2c91c432df24d8b8208
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-29 22:10:06 -0300

    First commit

---
+++ src/Exception/ViewTemporartyDirectoryNotFoundException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\View\Latte\Exception;
+
+    use Exception;
+
+
+    class ViewTemporartyDirectoryNotFoundException extends Exception
+    { }
---
+++ src/LatteTemplateHandler.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\View\Latte;
+
+    use STDW\Contract\ViewHandlerInterface;
+    use STDW\View\Latte\Exception\ViewTemporartyDirectoryNotFoundException;
+    use STDW\View\Latte\Exception\ViewFileNotFoundException;
+    use Latte\Engine;
+
+
+    class LatteTemplateHandler implements ViewHandlerInterface
+    {
+        protected Engine $latte;
+
+
+        public function __construct(string $temporary_directory)
+        {
+            if ( ! is_dir($temporary_directory) || ! file_exists($temporary_directory)) {
+                throw new ViewTemporartyDirectoryNotFoundException("View: Directory '{$temporary_directory}' not found");
+            }
+
+            $this->latte = new Engine();
+            $this->latte->setTempDirectory($temporary_directory);
+        }
+
+
+        public function compile(string $filepath, array $data = []): string
+        {
+            if ( ! file_exists($filepath)) {
+                throw new ViewFileNotFoundException("View: '{$filepath}' not found");
+            }
+
+            return $this->latte->renderToString($filepath, $data);
+        }
+
+        public function render(string $filepath, array $data = []): void
+        {
+            // if ( ! file_exists($filepath)) {
+            //     throw new ViewFileNotFoundException("View: '{$filepath}' not found");
+            // }
+
+            // $this->latte->render($filepath, $data);
+
+            echo $this->compile($filepath, $data);
+        }
+    }
